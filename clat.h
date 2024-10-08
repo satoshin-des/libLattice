54a950c6d77bc73f8c4541669509ff53aa362031
Nagato <transcendentalliouville@gmail.com>
2024-10-08 10:39:54 +0900

    作ります

--- clat.h
+++ clat.h
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct _lattice {
-    char *name;
-    char sex;
-    int age;
-    char *add;
-    char *job;
+    int nrows;
+    int ncols;
+    long **basis;
+    long double **basis_star;
+    long double *B;
+    long double **mu;
 } lattice;
+
+
+lattice random_lattice(int nrows, int ncols){
+    lattice b;
+    srand((unsigned int)time(NULL));
+    b.nrows = nrows;
+    b.ncols = ncols;
+    b.basis = (long **)malloc(nrows * sizeof(long *));
+    for(int i = 0, j; i < nrows; ++i){
+        b.basis[i] = (long *)malloc(ncols * sizeof(long));
+        for(j = 0; j < ncols; ++j)
+            b.basis[i][j] = rand();
+    }
+}
